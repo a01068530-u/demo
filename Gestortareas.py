a5568d7bf959456207b34e6bc80e60c72c9edf6a
Memo <a01068530@tec.mx>
2025-09-12 23:01:24 -0600

    Update and rename programa.py to Gestortareas.py

--- Gestortareas.py
+++ Gestortareas.py
@@ -1,34 +1,31 @@
 """
-Avance 3
-Algoritmo: Gestor de Tareas con operadores y funciones
+Avance 4
+Algoritmo: Gestor de Tareas con estructuras de decisión simples
 Descripción:
-Este algoritmo permite al usuario registrar tareas pendientes en una lista.
-Se pueden agregar nuevas tareas, mostrar las tareas actuales o finalizar el programa.
+El usuario puede agregar tareas, verlas o salir.
 """
 
 def agregar_tarea(lista, tarea):
-    lista = lista + [tarea]  
-    return lista
-
-
+    return lista + [tarea]
 def mostrar_tareas(lista):
-    if len(lista) == 0:
+    # Decisión según cuántas tareas hay
+    if lista == []:
         return "No hay tareas registradas"
-    elif len(lista) == 1:
+    elif lista[1:] == []:   # si solo hay 1 tarea
         return "1" + lista[0]
-    elif len(lista) == 2:
+    elif lista[2:] == []:   # si hay 2 tareas
         return "1" + lista[0] + "2" + lista[1]
-    else:
+    else:                   # si hay 3 o más
         return "1" + lista[0] + "2" + lista[1] + "3" + lista[2]
 
-tareas = []    
-seguir = 1      
+tareas = []
+seguir = 1
 
 while seguir == 1:
-    print("Menu")
-    print("1. Agregar Tarea")
-    print("2. Mostrar Tareas")
-    print("3. Salir")
+    print("Menú")
+    print("Agregar Tarea")
+    print("Mostrar Tareas")
+    print("Salir")
 
     opcion = int(input("Elige una opción: "))
 
@@ -42,8 +39,7 @@ while seguir == 1:
 
     elif opcion == 3:
         print("Programa finalizado")
-        seguir = 0   
+        seguir = 0
+
     else:
         print("Opción no válida")
-
-   
